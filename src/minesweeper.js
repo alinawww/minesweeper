ab199bc4a6d24c48b5b23c24b493d42a754948de
Alina Tudose <alina.tudose@travelbird.com>
2017-10-16 23:00:31 +0200

    generateboards

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,50 @@
-const printBoard = (board) => {
-    console.log(board[0].join('|'));
-    console.log(board[1].join('|'));
-    console.log(board[2].join('|'));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    const board = []
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = []
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(' ')
+        }
+        board.push(row)
+    }
+    return board
 }
-const board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-]
 
-printBoard(board)
 
-board[0][1] = 1
-board[2][2] = 'B'
-console.log('--------------');
-printBoard(board)
+generatePlayerBoard(5, 10)
+
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    const board = []
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = []
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(null)
+        }
+        board.push(row)
+    }
+
+    let numberOfBombsPlaced = 0
+    while (numberOfBombsPlaced <= numberOfBombs) {
+        // An important note: The code in your while loop has the potential to place bombs on top of already existing bombs.
+        // This will be fixed when you learn about control flow.
+        const randomRowIndex = Math.floor(Math.random() * numberOfRows)
+        const randomColumnIndex = Math.floor(Math.random() * numberOfColumns)
+        board[randomRowIndex][randomColumnIndex] = 'B'
+        numberOfBombsPlaced ++
+    }
+    return board
+}
+
+const printBoard = (board) => {
+    return board.map(row => row.join(' | ')).join('\n')
+}
+
+const playerBoard = generatePlayerBoard(3, 4)
+const bombBoard = generateBombBoard(3, 4, 5)
+
+console.log('Player board:');
+console.log(printBoard(playerBoard));
+
+console.log('Bomb board:');
+console.log(printBoard(bombBoard));
